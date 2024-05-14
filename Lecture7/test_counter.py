ba456b34501bcdadf6457350450bdac72c288fd9
pagrochowski <pagrochowski@gmail.com>
2024-05-14 19:05:56 +0000

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
    GITHUB ACTIONS
    
     Changes to be committed:
    	new file:   .devcontainer/.github/workflows/ci.yml
    	modified:   .devcontainer/dockerfile
    	modified:   Lecture7/test_counter.py
    	new file:   Lecture7/test_counter_original.py
    	renamed:    .devcontainer/requirements.txt -> requirements.txt

--- Lecture7/test_counter.py
+++ Lecture7/test_counter.py
@@ -3,18 +3,12 @@ import pathlib
 import unittest
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
-# Finds the Uniform Resourse Identifier of a file
 def file_uri(filename):
     return pathlib.Path(os.path.abspath(filename)).as_uri()
 
-# Sets up web driver using Google chrome
-driver = webdriver.Chrome()
-
-# Standard outline of testing class
 class WebpageTests(unittest.TestCase):
 
     @classmethod
@@ -23,37 +17,43 @@ class WebpageTests(unittest.TestCase):
         options.add_argument("--headless=new") 
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-gpu") 
+        options.binary_location = '/usr/bin/chromium-browser' 
 
-        cls.driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+        service = Service()
+        cls.driver = webdriver.Chrome(service=service, options=options)
 
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
 
     def test_title(self):
         """Make sure title is correct"""
-        driver.get(file_uri("counter.html"))
-        self.assertEqual(driver.title, "Counter")
+        self.driver.get(file_uri("counter.html"))
+        self.assertEqual(self.driver.title, "Counter")
 
     def test_increase(self):
         """Make sure header updated to 1 after 1 click of increase button"""
-        driver.get(file_uri("counter.html"))
-        increase = driver.find_element_by_id("increase")
+        self.driver.get(file_uri("counter.html"))
+        increase = self.driver.find_element("id", "increase")
         increase.click()
-        self.assertEqual(driver.find_element_by_tag_name("h1").text, "1")
+        self.assertEqual(self.driver.find_element("tag name", "h1").text, "1")
 
     def test_decrease(self):
         """Make sure header updated to -1 after 1 click of decrease button"""
-        driver.get(file_uri("counter.html"))
-        decrease = driver.find_element_by_id("decrease")
+        self.driver.get(file_uri("counter.html"))
+        decrease = self.driver.find_element("id", "decrease")
         decrease.click()
-        self.assertEqual(driver.find_element_by_tag_name("h1").text, "-1")
+        self.assertEqual(self.driver.find_element("tag name", "h1").text, "-1")
 
     def test_multiple_increase(self):
         """Make sure header updated to 3 after 3 clicks of increase button"""
-        driver.get(file_uri("counter.html"))
-        increase = driver.find_element_by_id("increase")
+        self.driver.get(file_uri("counter.html"))
+        increase = self.driver.find_element("id", "increase")
         for i in range(3):
             increase.click()
-        self.assertEqual(driver.find_element_by_tag_name("h1").text, "3")
+        self.assertEqual(self.driver.find_element("tag name", "h1").text, "3")
+
 
 if __name__ == "__main__":
     unittest.main()
